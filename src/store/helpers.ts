6769d8a0f74a859e6844cc030daefeb8627795f4
Kolade <olubanjok@gmail.com>
2026-01-11 12:49:50 +0100

    chore(store): add the live server url

--- src/store/helpers.ts
+++ src/store/helpers.ts
@@ -1,6 +1,6 @@
 import { fetchBaseQuery } from "@reduxjs/toolkit/query";
 
-const backendUrl = import.meta.env.VITE_TEST_BACKEND_URL;
+const backendUrl = import.meta.env.VITE_RETROJI_BACKEND_URL;
 
 const baseQuery = fetchBaseQuery({ baseUrl: backendUrl });
 
