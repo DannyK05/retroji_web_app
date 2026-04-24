4816f04f9acabc08389cc06f54344e78e47eecd6
Kolade <olubanjok@gmail.com>
2026-04-24 23:54:07 +0100

    fix: fix vite 404 error on hot reload; fix fallback profile pic not found on scoops card

--- src/modules/scoops/index.tsx
+++ src/modules/scoops/index.tsx
@@ -122,7 +122,7 @@ export default function Scoop() {
                   name={author.username}
                   content={content}
                   date={created_at}
-                  image={"/public/assets/images/profile_pic.jpg"}
+                  image={"/assets/images/profile_pic.jpg"}
                   likeCount={like_count}
                   isLiked={is_liked}
                   repliesCount={replies_count}
