bd932299c81af30214c1713f8219e331cea9dce4
Kolade <olubanjok@gmail.com>
2026-05-01 16:21:29 +0100

    feat:(search): implement a ProfileCard

--- src/components/core/search-section/components/ProfileTab.tsx
+++ src/components/core/search-section/components/ProfileTab.tsx
@@ -1,3 +1,4 @@
+import ProfileCard from "../../../../modules/search/components/ProfileCard";
 import EmptyScreen from "../../../common/empty-screen";
 import { ProfileTabProps } from "../types";
 
@@ -5,7 +6,16 @@ export default function ProfileTab({ data }: ProfileTabProps) {
   return (
     <div className="w-full flex flex-col items-center space-y-3 py-2 px-1 lg:max-h-[450px] lg:px-3 lg:overflow-y-auto">
       {data && data.length > 0 ? (
-        data.map(({ id, user }) => <div key={id}>{user.username}</div>)
+        data.map(({ id, user }) => (
+          <ProfileCard
+            imageUrl="/assets/images/profile_pic.jpg"
+            key={id}
+            userId={user.id}
+            username={user.username}
+            followers={user.followers}
+            following={user.following}
+          />
+        ))
       ) : (
         <EmptyScreen />
       )}
